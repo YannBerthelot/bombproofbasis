44178421efc25c10da80735fdc3d06d0fe66aa67
YANN BERTHELOT <yann.berthelot@capgemini.com>
2022-12-06 15:16:11 +0100

    added config tests

--- src/bombproofbasis/utils/config.py
+++ src/bombproofbasis/utils/config.py
@@ -3,65 +3,88 @@ Various utilities
 """
 import argparse
 import configparser
-import os
 import platform
-from pathlib import Path
+from typing import List
 
 import torch
 
 
-def read_config(file: Path = None) -> configparser.ConfigParser:
+def parse_args(args: List[str]) -> argparse.Namespace:
     """
-    Read config file, if None is provided uses the default config file.
+    Parse args from system input
 
     Args:
-        file (Path, optional): The path to config file. Defaults to None.
+        args (List[str]): List of flags and values (e.g. \
+            ["--config", "config.ini"])
+
+    Returns:
+        argparse.Namespace: The parsed arguments in a Namespace
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config")
+    return parser.parse_args(args)
+
+
+def read_config(config_file) -> configparser.ConfigParser:
+    """
+    Read config file and output readable config.
+
+    Args:
+        config_file (Path): The path to config file.
 
     Raises:
         ValueError: If the config file is not in .ini format.
-        OSError: _description_
 
     Returns:
-        configparser.ConfigParser: _description_
+        configparser.ConfigParser: the config
     """
-    if file is None:
-        parse = argparse.ArgumentParser()
-        parse.add_argument("-s")
-        args = parse.parse_args()
+    # if file is None:
+    #     arg_inputs = sys.argv[1:]
+    #     print("arg_inputs", arg_inputs)
+    #     try:
+    #         args = parse_args(arg_inputs)
+    #     except SystemExit:
+    #         args = None
+    #     if args is None:
+    #         print("Using default config")
 
-        if args is None:
-            print("Using default config")
-            print(os.listdir())
-            folder = os.path.dirname(__file__)
-            config_file = Path(os.path.join(folder, "config.ini"))
-        else:
-            config_file = args.s
-    else:
-        config_file = file
+    #         base_folder = os.path.dirname(__file__)
+    #         folder = os.path.join(base_folder, "../tests")
+    #         print(os.path.join(folder, "config.ini"))
+    #         config_file = Path(os.path.join(folder, "config.ini"))
+    #     else:
+    #         config_file = args.s
+    # else:
+    #     config_file = file
 
-    if not str(config_file).rsplit(".", maxsplit=1)[-1] == (".ini"):
+    if "." in str(config_file) and (
+        not str(config_file).rsplit(".", maxsplit=1)[-1] == ("ini")
+    ):
         raise ValueError(
             f"Configuration file {config_file} is in the \
                 {str(config_file).rsplit('.', maxsplit=1)[-1]} extension, \
                     should be .ini"
         )
-    try:
-        config = configparser.ConfigParser()
-        config.read(config_file)
-    except OSError as exc:
-        raise OSError(f"Config file {config_file} is impossible to read") from exc
-    if torch.cuda.is_available():
-        GPU_NAME = torch.cuda.get_device_name(0)
-    else:
-        GPU_NAME = "No GPU"
+    if "." not in str(config_file):
+        raise ValueError(
+            f"File {config_file} impossible to read, it doesn't have any extension"
+        )
+
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    GPU_NAME = get_GPU_name()
     config["HARDWARE"]["GPU_name"] = GPU_NAME
     config["HARDWARE"]["CPU_name"] = platform.processor()
     return config
 
 
-if __name__ == "__main__":
-    parse = argparse.ArgumentParser()
-    parse.add_argument("-s")
-    args = parse.parse_args()
-    config = read_config(args.s)
-    print(config.sections())
+def get_GPU_name() -> str:
+    """
+    Get GPU name from the system
+
+    Returns:
+        str: The GPU name or No GPU
+    """
+    if torch.cuda.is_available():
+        return torch.cuda.get_device_name(0)
+    return "No GPU"
---
+++ tests/test_config.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+import pytest
+
+from bombproofbasis.utils.config import parse_args, read_config
+
+
+def test_parse_args():
+    assert (parse_args(["--config", "config.ini"]).config) == "config.ini"
+
+
+def test_config():
+    # check if default config file exists
+    folder = os.path.dirname(__file__)
+    config_file = Path(os.path.join(folder, "config.ini"))
+    assert os.path.exists(config_file)
+    assert read_config(config_file) is not None
+    with pytest.raises(ValueError):
+        read_config("nonsense.xlsx")
+    with pytest.raises(ValueError):
+        read_config("nonsense")
